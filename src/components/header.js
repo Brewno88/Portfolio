40d79977f81d9e78e0ab4c1291f9842dc26cb969
Vincenzo <ilcontevince@gmail.com>
2020-03-09 18:42:34 +0000

    modify menu bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,80 +1,75 @@
 import { Link } from "gatsby"
-// import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React from "react"
 import styled from "styled-components"
 
 const Header = ({ ...props }) => {
-  const [isNavbarOpen, setIsNavbarOpen] = useState(true)
-
   return (
-    <MyHeader className="header" isNavbarOpen={isNavbarOpen}>
+    <MyHeader className="header">
       <Link to="/">
-        <MyToggle
-          onClick={() => setIsNavbarOpen(!isNavbarOpen)}
-          isNavbarOpen={isNavbarOpen}
-        >
+        <MyToggle>
           <div className="navbar-toggle">
             <span className="V">V</span>
             <span className="C">C</span>
           </div>
+          <Navbar className="navbar">
+            <ol>
+              <Link
+                to="/about/"
+                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+              >
+                <li className="nav-item">About Me</li>
+              </Link>
+              <Link
+                to="/projects/"
+                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+              >
+                <li className="nav-item">Projects</li>
+              </Link>
+              <Link
+                to="/skills/"
+                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+              >
+                <li className="nav-item">Skills</li>
+              </Link>
+              <Link
+                to="/get-in-touch/"
+                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+              >
+                <li className="nav-item">Get In Touch</li>
+              </Link>
+            </ol>
+          </Navbar>
         </MyToggle>
       </Link>
-      {isNavbarOpen ? (
-        <Navbar>
-          <ol>
-            <Link
-              to="/about/"
-              activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-            >
-              <li className="nav-item">About Me</li>
-            </Link>
-            <Link
-              to="/projects/"
-              activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-            >
-              <li className="nav-item">Projects</li>
-            </Link>
-            <Link
-              to="/skills/"
-              activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-            >
-              <li className="nav-item">Skills</li>
-            </Link>
-            <Link
-              to="/get-in-touch/"
-              activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-            >
-              <li className="nav-item">Get In Touch</li>
-            </Link>
-          </ol>
-        </Navbar>
-      ) : (
-        <div></div>
-      )}
     </MyHeader>
   )
 }
-// Header.propTypes = {
-//   siteTitle: PropTypes.string,
-// }
-
-// Header.defaultProps = {
-//   siteTitle: ``,
-// }
 
 const MyHeader = styled.header`
   width: auto;
   display: flex;
-  justify-content: ${props => (props.isNavbarOpen ? "space-between" : "end")};
   padding: 1rem 1rem 0 1rem;
 `
 
 const Navbar = styled.nav`
   background-color: var(--off-white);
-  padding: 2rem 2rem;
+  padding: 2rem 3rem 2rem 2rem;
   border-radius: 5rem;
-  display: flex;
+  height: 100%;
   align-items: center;
+  display: none;
+  transform: translateX(1.5rem);
+
+  @keyframes appear {
+    from {
+      opacity: 0;
+      transform: scaleY(0) translateX(8rem);
+    }
+    to {
+      opacity: 1;
+      transform: scaleY(1) translateX(1.5rem);
+    }
+  }
 
   ol {
     display: flex;
@@ -82,9 +77,11 @@ const Navbar = styled.nav`
 
     a:hover {
       text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
+      transition: all 0.3s linear;
     }
 
     a {
+      transform: all linear 1s;
       color: var(--gunmetal);
       text-shadow: 0.2rem 0.2rem 0.1rem var(--light-bright-green);
       font-size: 1.6rem;
@@ -100,11 +97,19 @@ const Navbar = styled.nav`
 
 const MyToggle = styled.div`
   display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
   position: absolute;
   bottom: 3%;
   right: 3%;
 
+  &:hover .navbar {
+    display: flex;
+    animation: appear 0.5s;
+  }
+
   .navbar-toggle {
+    z-index: 10;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -115,14 +120,12 @@ const MyToggle = styled.div`
   }
   .V {
     text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
-    font-family: Krungthep;
     font-size: 4rem;
     text-align: left;
     color: var(--light-bright-green);
   }
   .C {
     text-shadow: 0.2rem 0.2rem 0.1rem var(--light-bright-green);
-    font-family: Krungthep;
     font-size: 4rem;
     color: var(--pink);
   }
