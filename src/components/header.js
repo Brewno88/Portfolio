7206764557a661dbb2384ae4a6f1b651d1b97a75
Vincenzo <ilcontevince@gmail.com>
2020-03-11 12:04:26 +0000

    add animation and home button

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,64 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 
 const Header = ({ ...props }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  console.log(isOpen)
   return (
     <MyHeader className="header">
-      <Link to="/">
-        <MyToggle>
-          <div className="navbar-toggle">
-            <span className="V">V</span>
-            <span className="C">C</span>
-          </div>
-          <Navbar className="navbar">
-            <ol>
-              <Link
-                to="/about/"
-                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-              >
-                <li className="nav-item">About Me</li>
-              </Link>
-              <Link
-                to="/projects/"
-                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-              >
-                <li className="nav-item">Projects</li>
-              </Link>
-              <Link
-                to="/skills/"
-                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-              >
-                <li className="nav-item">Skills</li>
-              </Link>
-              <Link
-                to="/get-in-touch/"
-                activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
-              >
-                <li className="nav-item">Get In Touch</li>
-              </Link>
-            </ol>
-          </Navbar>
-        </MyToggle>
-      </Link>
+      <MyToggle onClick={() => setIsOpen(!isOpen)} isOpen={isOpen}>
+        <div className="navbar-toggle">
+          <span className="V">V</span>
+          <span className="C">C</span>
+        </div>
+
+        {isOpen ? (
+          <>
+            <Navbar className="navbar">
+              <ol>
+                <li className="nav-item">
+                  <Link
+                    to="/about/"
+                    activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+                  >
+                    About Me
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/projects/"
+                    activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/skills/"
+                    activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+                  >
+                    Skills
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    to="/get-in-touch/"
+                    activeStyle={{ textShadow: "2px 2px 1px var(--pink)" }}
+                  >
+                    Get In Touch
+                  </Link>
+                </li>
+              </ol>
+            </Navbar>
+            <Link to="/">
+              <Home>🏠</Home>
+            </Link>
+          </>
+        ) : (
+          <div></div>
+        )}
+      </MyToggle>
     </MyHeader>
   )
 }
@@ -51,29 +69,61 @@ const MyHeader = styled.header`
   padding: 1rem 1rem 0 1rem;
 `
 
-const Navbar = styled.nav`
-  background-color: var(--off-white);
-  padding: 2rem 3rem 2rem 2rem;
+const Home = styled.div`
+  position: absolute;
+  background: white;
   border-radius: 5rem;
-  height: 100%;
+  width: 4rem;
+  height: 4rem;
+  right: 100%;
+  bottom: 15%;
+  display: ${props => (props.isOpen ? "none" : "flex")};
+  justify-content: center;
   align-items: center;
-  display: none;
-  transform: translateX(1.5rem);
+  font-size: 2rem;
+  animation: homeAppear 0.5s;
 
-  @keyframes appear {
+  @keyframes homeAppear {
     from {
-      opacity: 0;
-      transform: scaleY(0) translateX(8rem);
+      transform: scaleY(0);
+      right: 50%;
     }
     to {
-      opacity: 1;
-      transform: scaleY(1) translateX(1.5rem);
+      transform: scaleY(1);
+      right: 100%;
     }
   }
+`
+
+const Navbar = styled.nav`
+  height: 100%;
+  align-items: center;
+  display: ${props => (props.isOpen ? "none" : "flex")};
+  z-index: 10;
 
   ol {
     display: flex;
+    flex-direction: column;
+    align-items: flex-end;
     margin: 0;
+    position: absolute;
+    right: 0;
+    width: 15rem;
+    bottom: 100%;
+    animation: navAppear 0.3s;
+
+    @keyframes navAppear {
+      from {
+        bottom: -50%;
+
+        transform: scaleY(0);
+      }
+      to {
+        bottom: 100%;
+
+        transform: scaleY(1);
+      }
+    }
 
     a:hover {
       text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
@@ -85,6 +135,7 @@ const Navbar = styled.nav`
       color: var(--gunmetal);
       text-shadow: 0.2rem 0.2rem 0.1rem var(--light-bright-green);
       font-size: 1.6rem;
+      margin: 1rem;
     }
   }
 
@@ -92,41 +143,39 @@ const Navbar = styled.nav`
     list-style: none;
     margin: 0 1rem;
     font-family: var(--main-font);
+    background-color: var(--off-white);
+    padding: 0.5rem 0.5rem;
+    border-radius: 5rem;
   }
 `
 
 const MyToggle = styled.div`
   display: flex;
-  align-items: center;
-  flex-direction: row-reverse;
+  align-items: flex-end;
+  flex-direction: column-reverse;
   position: absolute;
   bottom: 3%;
   right: 3%;
-
-  &:hover .navbar {
-    display: flex;
-    animation: appear 0.5s;
-  }
 
   .navbar-toggle {
     z-index: 10;
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 8rem;
-    height: 8rem;
+    width: 6rem;
+    height: 6rem;
     border-radius: 7.5rem;
     background-color: var(--off-white);
   }
   .V {
     text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
-    font-size: 4rem;
+    font-size: 3rem;
     text-align: left;
     color: var(--light-bright-green);
   }
   .C {
     text-shadow: 0.2rem 0.2rem 0.1rem var(--light-bright-green);
-    font-size: 4rem;
+    font-size: 3rem;
     color: var(--pink);
   }
 `
