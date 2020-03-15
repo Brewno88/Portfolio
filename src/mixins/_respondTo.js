83f086082d46804fe34bd7f1d94749b490b66b00
Vincenzo <ilcontevince@gmail.com>
2020-03-15 17:44:58 +0000

    add also ladscape

--- src/mixins/_respondTo.js
+++ src/mixins/_respondTo.js
@@ -4,7 +4,9 @@ import { breakpoints } from "./_variables"
 export const respondTo = Object.keys(breakpoints).reduce(
   (accumulator, label) => {
     accumulator[label] = (...args) => css`
-      @media (min-width: ${breakpoints[label]}) {
+      @media (min-width: ${breakpoints[label]}) and (min-height: ${breakpoints[
+          label
+        ]}) {
         ${css(...args)};
       }
     `
