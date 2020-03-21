f4f5eddaf9af775ab97165e6ab66f7fc5ae7fae2
Vincenzo <ilcontevince@gmail.com>
2020-03-21 16:37:37 +0000

    change action="thank-you"

--- src/pages/get-in-touch.js
+++ src/pages/get-in-touch.js
@@ -14,7 +14,7 @@ const GetInTouch = ({ ...props }) => {
         <Main className="getInTouch-main">
           <form
             method="POST"
-            action="/thanks/"
+            action="thank-you"
             form-name="contact"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
