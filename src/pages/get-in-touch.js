86b2249aec57ef688999f89b3cb21db4ef109bc0
Vincenzo <ilcontevince@gmail.com>
2020-03-21 12:00:23 +0000

    cleared some stuff

--- src/pages/get-in-touch.js
+++ src/pages/get-in-touch.js
@@ -1,20 +1,23 @@
 import React from "react"
 import styled from "styled-components"
-import { respondTo } from "../mixins/respondTo"
 
 import Layout from "../components/layout"
 import Main from "../components/main"
 import SEO from "../components/seo"
+import { respondTo } from "../mixins/respondTo"
 
 const GetInTouch = ({ ...props }) => {
   return (
     <Wrap>
-      <Layout>
+      <Layout path={props.path}>
         <SEO title="Page two" />
         <Main className="getInTouch-main">
-          <form action="POST" data-netlify="true">
-            <input name="email" type="email" />
-            <input name="submit" type="submit" />>
+          <form method="post" name="contact" data-netlify="true">
+            <h1>Your Email</h1>
+            <input type="email" name="email" />
+            <h1>Message</h1>
+            <textarea type="message" name="message" className="message" />
+            <button type="submit">Send</button>
           </form>
         </Main>
       </Layout>
@@ -24,23 +27,6 @@ const GetInTouch = ({ ...props }) => {
 
 const Wrap = styled.div`
   height: 100%;
-
-  /* .some {
-    color: var(--light-bright-green);
-    text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
-    font-size: 2rem;
-
-    input {
-      border-radius: 5rem;
-      border: none;
-      width: 100%;
-      height: 2rem;
-      padding: 2rem;
-      margin: 3rem 0;
-      font-size: 1.5rem;
-    }
-
-  }
 
   form {
     width: 50%;
@@ -54,20 +40,32 @@ const Wrap = styled.div`
       `}
     ${respondTo.tablet`
       width: 80%;
-    `}
+      `}
     ${respondTo.laptop`
       width: 40%;
-    `}
+      `}
     ${respondTo.laptopL`
       width: 40%;
-    `}
+      `}
+
+  h1 {
+    color: var(--light-bright-green);
+    text-shadow: 0.2rem 0.2rem 0.1rem var(--pink);
+    font-size: 2rem;
+  }
+
+  input {
+    border-radius: 5rem;
+    border: none;
+    width: 100%;
+    height: 2rem;
+    padding: 2rem;
+    margin: 3rem 0;
+    font-size: 1.5rem;
   }
 
   .message {
-    display: flex;
-    flex-direction: column;
-      height: 50%;
-    textarea {
+
       border: none;
       border-radius: 2rem;
       padding: 2rem;
@@ -75,9 +73,10 @@ const Wrap = styled.div`
       font-size: 1.5rem;
       resize: none;
       width: 100%;
-      height: 100%;
-    }
+      height:50%;
+
   }
+
     button {
       font-size: 1.5rem;
       border-radius: 50px;
@@ -93,7 +92,8 @@ const Wrap = styled.div`
         transform: translate3D(0.2rem, 0.2rem, 0.2rem);
         /* transform: translateY(3px); */
       }
-    } */
+    }
+  }
 `
 
 export default GetInTouch
